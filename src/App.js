0fd392a0c69acddeceff9407e9d5256ac5456d2e
Link1986 <ali.ghoudi@gmail.com>
2020-07-13 21:51:41 +0200

    modified picture and colors, added some text and reworked the structure

--- src/App.js
+++ src/App.js
@@ -5,7 +5,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div className="main_content">
+        <div className="main_content">
+            <div class="picture">
+                <img  src={logo} alt="cv_picture"/>
+            </div>
+            <div class="work">
+                <h1><span>&#9646;</span> ALI GHOUDI</h1>
+                <p class="description">Développeur Web Fullstack</p>
+            </div>
+        </div>
+      {/* <div className="main_content">
           <div class="picture">
                 <img width="100%" height="260px"  src={logo} alt="cv_picture"/>
            </div>
@@ -13,7 +22,8 @@ function App() {
                 <h1>ALI GHOUDI</h1>
                 <p class="description">Développeur Web Fullstack</p>
             </div>
-      </div>        
+           
+      </div>         */}
     </div>
   );
 }
