cb8f31979c72cfac224413aff9bb4d9f1aaab0ac
Link1986 <ali.ghoudi@gmail.com>
2020-07-14 21:06:37 +0200

    added some informations, need to restructure the codebase

--- src/App.js
+++ src/App.js
@@ -6,14 +6,20 @@ function App() {
   return (
     <div className="App">
         <div className="main_content">
-            <div class="picture">
+            <div className="picture">
                 <img  src={logo} alt="cv_picture"/>
             </div>
-            <div class="work">
+            <div className="work">
                 <h1><span>&#9646;</span> ALI GHOUDI</h1>
-                <p class="description">Développeur Web Fullstack</p>
+                <p className="description">Développeur Web Fullstack</p>
+                <p><span className="phone">&#9743;</span> 07 - 82 - 18 - 10 -34</p>
+                <p><span className="mail">&#9993;</span> ali.ghoudi@gmail.com</p>
+                <p><span className="home" role="img" aria-label="house">&#127969;</span> SAINT-ÉTIENNE, FRANCE</p>
+                
             </div>
+            <hr/>
         </div>
+        
       {/* <div className="main_content">
           <div class="picture">
                 <img width="100%" height="260px"  src={logo} alt="cv_picture"/>
