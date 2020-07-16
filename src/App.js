c65bcac9de006a24aae3bd28e31c3ea1ad1bc08a
Link1986 <ali.ghoudi@gmail.com>
2020-07-16 21:52:45 +0200

    added mes compétences and mes atouts to my resume

--- src/App.js
+++ src/App.js
@@ -6,18 +6,39 @@ function App() {
   return (
     <div className="App">
         <div className="main_content">
-            <div className="picture">
-                <img  src={logo} alt="cv_picture"/>
-            </div>
-            <div className="work">
-                <h1><span>&#9646;</span> ALI GHOUDI</h1>
-                <p className="description">Développeur Web Fullstack</p>
-                <p><span className="phone">&#9743;</span> 07 - 82 - 18 - 10 -34</p>
-                <p><span className="mail">&#9993;</span> ali.ghoudi@gmail.com</p>
-                <p><span className="home" role="img" aria-label="house">&#127969;</span> SAINT-ÉTIENNE, FRANCE</p>
-                
-            </div>
+            <section className="presentation">
+              <div className="picture">
+                  <img  src={logo} alt="cv_picture"/>
+              </div>
+              <div className="work">
+                  <h1><span>&#9646;</span> ALI GHOUDI</h1>
+                  <p className="description">Développeur Web Fullstack</p>
+                  <p><span className="phone">&#9743;</span> 07 - 82 - 18 - 10 -34</p>
+                  <p><span className="mail">&#9993;</span> ali.ghoudi@gmail.com</p>
+                  <p><span className="home" role="img" aria-label="house">&#127969;</span> SAINT-ÉTIENNE, FRANCE</p>                
+              </div>
+            </section>            
             <hr/>
+            <section className="competences">
+              <div className="mesAttouts">
+                  <h2><span>&#10024;</span>MES ATOUTS</h2>
+                  <ul>
+                    <li>Fiable</li>
+                    <li>Organisé</li>
+                    <li>Rigoureux</li>
+                    <li>Créatif</li>
+                    <li>Empathique</li>
+                  </ul>
+              </div>
+              <div className="mesCompetences">
+                      <h2><span>&#127919;</span>MES COMPÉTENCES</h2>
+                        <p>Réaliser l'intégration d 'une maquette graphique</p>
+                        <p>Organisé</p>
+                        <p>Rigoureux</p>
+                        <p>Créatif</p>
+                        <p>Empathique</p>           
+              </div>
+            </section>
         </div>
         
       {/* <div className="main_content">
