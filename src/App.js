718f3c82672b3202bf906d2f67687a33e49a1cf4
Link1986 <ali.ghoudi@gmail.com>
2020-07-23 22:02:02 +0200

    added languages with logo, and reworked code to make it more clean

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import './App.css';
 import TopResume from './components/TopResume/TopResume';
 import Main from './components/Main/Main';
 import Competences from './components/Competences/Competences';
+import javascript from './assets/javascript.jpg';
+import php from './assets/php.png';
+import mysql from './assets/mysql.png';
+import symfony from './assets/symfony.jpg';
+import react from './assets/react.png';
+import node from './assets/node.png';
 
 class App extends React.Component {
 
@@ -14,51 +20,27 @@ class App extends React.Component {
         }
     }
 
-    blue = () => {
+    handleClick = (event) => {
+        const appColor = event.target.getAttribute('data-appcolor');
+        const mainColor = event.target.getAttribute('data-maincolor');
         this.setState({
-            appColor: '#2056ac',
-            mainColor: '#e3f0f8'
-        })
-        localStorage.setItem("appColor", "#2056ac");
-        localStorage.setItem("mainColor", "#e3f0f8");
-    }
-
-    yellow = () => {
-        this.setState({
-            appColor: '#ffbe4a',
-            mainColor: '#fef5e6'
-        })
-        localStorage.setItem("appColor", "#ffbe4a");
-        localStorage.setItem("mainColor", "#fef5e6");
-    }
-
-    green = () => {
-        this.setState({
-            appColor: '#174545',
-            mainColor: '#6d9190'
-        })
-        localStorage.setItem("appColor", "#174545");
-        localStorage.setItem("mainColor", "#6d9190");
-    }
-
-    red = () => {
-        this.setState({
-            appColor: '#de4b4c',
-            mainColor: '#dd7381'
-        })
-        localStorage.setItem("appColor", "#de4b4c");
-        localStorage.setItem("mainColor", "#dd7381");
+            appColor: appColor,
+            mainColor: mainColor
+        });
+        localStorage.setItem("appColor", appColor);
+        localStorage.setItem("mainColor", mainColor);
     }
 
     render() {
-        return (
+        console.log('render')
+        return (            
             <div className="app" style={{backgroundColor: this.state.appColor}}>
                 <Main  color={this.state.mainColor}>
                     <div className="change_color">
-                        <div className="color_one" onClick={this.yellow}></div>
-                        <div className="color_two" onClick={this.green}></div>
-                        <div className="color_three" onClick={this.red}></div>
-                        <div className="color_four" onClick={this.blue}></div>
+                        <div className="color_one" onClick={this.handleClick} data-appcolor="#ffbe4a" data-maincolor="#fef5e6"></div>
+                        <div className="color_two" onClick={this.handleClick} data-appcolor="#174545" data-maincolor="#6d9190"></div>
+                        <div className="color_three" onClick={this.handleClick} data-appcolor="#de4b4c" data-maincolor="#dd7381"></div>
+                        <div className="color_four" onClick={this.handleClick} data-appcolor="#2056ac" data-maincolor="#e3f0f8"></div>
                     </div> 
                     <TopResume />           
                     <hr/>
@@ -67,6 +49,12 @@ class App extends React.Component {
                     <section className="formations">
                         <div className="langage">
                             <h2><span role="img" aria-label="outils favoris">&#128150;</span>MES OUTILS FAVORIS</h2>
+                            <div className="item"><img className="item_img" src={javascript} alt=""/>JAVASCRIPT</div>
+                            <div className="item"><img className="item_img" src={php} alt=""/>PHP</div>
+                            <div className="item"><img className="item_img" src={mysql} alt=""/>MYSQL</div>
+                            <div className="item"><img className="item_img" src={symfony} alt=""/>SYMFONY</div>
+                            <div className="item"><img className="item_img" src={react} alt=""/>REACT</div>
+                            <div className="item"><img className="item_img" src={node} alt=""/>NODE JS</div>
                         </div>
                         <div className="ma_formation">
                             <h2><span role="img" aria-label="formation">&#127891;</span>MA FORMATION</h2>
